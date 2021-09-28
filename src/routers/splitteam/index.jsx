3ffa0dea65eabfdbb70499cf1a85467d291d953c
jc <104832291@qq.com>
2021-09-28 14:44:15 +0800

    增加按钮的功能和删除按钮的功能

--- src/routers/splitteam/index.jsx
+++ src/routers/splitteam/index.jsx
@@ -1,12 +1,29 @@
 import { Button, Input, message, Modal, Space, Form, Typography } from "antd";
+import { LineOutlined, PlusOutlined } from "@ant-design/icons";
 import { useEffect, useState } from "react";
 import { players } from "../../assets/data";
 import splitTeam from "../../utils/index";
 import moment from "moment";
 
-moment.locale();
-
 const { Paragraph } = Typography;
+
+const formItemLayout = {
+	labelCol: {
+		xs: { span: 24 },
+		sm: { span: 4 },
+	},
+	wrapperCol: {
+		xs: { span: 24 },
+		sm: { span: 20 },
+	},
+};
+
+const formItemLayoutWithOutLabel = {
+	wrapperCol: {
+		xs: { span: 24, offset: 0 },
+		sm: { span: 20, offset: 4 },
+	},
+};
 
 const SplitTeam = () => {
 	// 数组或者对象若要及时更新，则需要传入一个新的数组或对象，不能传入原来的
@@ -14,7 +31,6 @@ const SplitTeam = () => {
 	const [result, setResult] = useState(null);
 	const [time, setTime] = useState(moment().format("YYYY-MM-DD HH:mm:ss"));
 	const [showModifyModal, setShowModifyModal] = useState(false);
-	const [form] = Form.useForm();
 
 	useEffect(() => {
 		const timeId = setInterval(() => {
@@ -23,31 +39,16 @@ const SplitTeam = () => {
 		return () => clearInterval(timeId);
 	}, []);
 
-	const handleDelete = () => {
-		message.info("该功能尚未实现");
-	};
-
-	const handleAdd = () => {
-		message.info("该功能尚未实现");
-	};
-
 	const handleModify = () => {
 		setShowModifyModal(true);
-		const fieldsValue = {};
-		cachePlayers.forEach((player) => {
-			fieldsValue[player] = player;
-		});
-		form.setFieldsValue(fieldsValue);
 	};
 
-	const handleModifyChange = async () => {
-		const res = await form.validateFields().catch((e) => {});
-		if (res !== undefined) {
-			const newPlayers = Object.values(res);
-			setCachePlayers(newPlayers);
-			Object.assign(players, newPlayers);
-			setShowModifyModal(false);
-		}
+	const handleModifyChange = (values) => {
+		const { playerValues } = values;
+		const res = playerValues.map((player) => player.value);
+		setCachePlayers([...res]);
+		Object.assign(players, res);
+		setShowModifyModal(false);
 	};
 
 	const generateTeam = () => {
@@ -70,15 +71,9 @@ const SplitTeam = () => {
 	const renderButtons = () => {
 		return (
 			<Space direction="vertical" size="large">
-				<Space size="large">
-					<Button type="ghost" onClick={handleModify}>
+				<Space style={{ marginTop: 20 }}>
+					<Button type="ghost" style={{ width: 240 }} onClick={handleModify}>
 						修改
-					</Button>
-					<Button type="primary" onClick={handleAdd}>
-						增加
-					</Button>
-					<Button type="danger" onClick={handleDelete}>
-						删除
 					</Button>
 				</Space>
 				<Space style={{ marginTop: 20, marginBottom: 50 }}>
@@ -127,6 +122,79 @@ const SplitTeam = () => {
 		);
 	};
 
+	const renderFieldSet = (cachePlayers) => {
+		const playerValues = [];
+		cachePlayers.forEach((player, index) => {
+			playerValues.push({
+				key: index,
+				fieldKey: index,
+				name: index,
+				value: player,
+			});
+		});
+		return (
+			<Form
+				{...formItemLayoutWithOutLabel}
+				initialValues={{ playerValues }}
+				onFinish={handleModifyChange}
+			>
+				<Form.List name="playerValues">
+					{(fields, { add, remove }) => (
+						<>
+							{fields.map((field, index) => (
+								<Form.Item
+									{...formItemLayout}
+									label={`玩家${index + 1}`}
+									required={false}
+									key={field.key}
+								>
+									<Form.Item
+										{...field}
+										name={[field.name, "value"]}
+										fieldKey={[field.fieldKey, "value"]}
+										validateTrigger={["onChange", "onBlur"]}
+										rules={[
+											{ required: true, message: "必填项", whitespace: true },
+										]}
+										noStyle
+									>
+										<Input style={{ width: "90%", marginRight: 10 }} />
+									</Form.Item>
+									{fields.length > 6 && (
+										<Button
+											shape="circle"
+											size="small"
+											icon={<LineOutlined />}
+											onClick={() => remove(field.name)}
+											danger
+										/>
+									)}
+								</Form.Item>
+							))}
+							<Form.Item>
+								{fields.length < 10 && (
+									<Button
+										type="dashed"
+										onClick={() => add()}
+										style={{ width: "90%" }}
+										icon={<PlusOutlined />}
+									>
+										新增玩家
+									</Button>
+								)}
+							</Form.Item>
+						</>
+					)}
+				</Form.List>
+				<Form.Item>
+					<Button type="primary" htmlType="submit" style={{ width: "90%" }}>
+						确定
+					</Button>
+				</Form.Item>
+			</Form>
+		);
+	};
+
 	return (
 		<div>
 			{renderHeader()}
@@ -135,25 +203,14 @@ const SplitTeam = () => {
 			{renderResults()}
 			<Modal
 				visible={showModifyModal}
-				onCancel={() => setShowModifyModal(false)}
-				onOk={handleModifyChange}
 				title="修改玩家"
-				forceRender
+				onCancel={() => {
+					setShowModifyModal(false);
+				}}
+				footer={null}
+				destroyOnClose
 			>
-				<Space direction="vertical" style={{ width: "100%" }}>
-					<Form form={form}>
-						{cachePlayers.map((player, index) => (
-							<Form.Item
-								label={`玩家${index + 1}`}
-								key={index}
-								name={player}
-								rules={[{ required: true, message: "请输入" }]}
-							>
-								<Input />
-							</Form.Item>
-						))}
-					</Form>
-				</Space>
+				{renderFieldSet(cachePlayers)}
 			</Modal>
 		</div>
 	);
